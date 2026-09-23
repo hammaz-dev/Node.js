a2b5645e49c7bd98373fc58b4e8302e54b920f01
hammaz-dev <mohdhammaz@192.168.1.18>
2026-09-23 23:21:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+fs.writeFileSync("hammaz.txt", "my name is hammaz");
+
+
+
+// console.log(20+20)
+
+// function fruit(item){
+//     console.log("fruit name is ",item);
+// }
+
+// fruit("apple");
+
+// var a = 10
+// var b = 5
+// console.log(a+b)
+
+
+
